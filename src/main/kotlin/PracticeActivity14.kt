9df4d2d413f60fb792a74121e0663f09f33e9694
Bernadette <bernadettedejesu99@gmail.com>
2023-04-17 20:21:13 +0800

    updated file

--- src/main/kotlin/PracticeActivity14.kt
+++ src/main/kotlin/PracticeActivity14.kt
@@ -8,8 +8,8 @@ fun main() {
     year = readln().toInt()
 
     //Process
-    if (year % 4 == 0 && year % 100 != 0) {
-        isLeapYear = year % 400 != 0
+    if (year % 4 == 0) {
+        isLeapYear = year % 100 != 0 || year % 400 == 0
     }else{
         isLeapYear =false
     }
