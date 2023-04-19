ac8e39e2dc13d70874bbdc3c18f9b6055a5fa6cf
Bernadette <bernadettedejesu99@gmail.com>
2023-04-19 19:34:49 +0800

    commit updated files

---
+++ src/main/kotlin/KotlinProgram15.kt
@@ -0,0 +1,14 @@
+fun main(){
+
+    //while loop
+    //1:loop variable
+    var counter = 0
+    //2:condition
+    while(counter < 30){
+        println("Bernadette")
+        //body
+        //3:Update
+        counter++
+    }
+    println("Hello World")
+}
