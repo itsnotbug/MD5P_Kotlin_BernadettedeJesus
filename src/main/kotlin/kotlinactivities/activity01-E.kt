d09ed1c4657b48469c125ea68e66b014d537c23a
Bernadette <bernadettedejesus99@gmail.com>
2023-05-10 01:52:15 +0800

    updated

--- src/main/kotlin/kotlinactivities/activity01-E.kt
+++ src/main/kotlin/kotlinactivities/activity01-E.kt
@@ -3,4 +3,44 @@ fun main(){
 //    Identify 3 use cases that will efficiently use an Array, ArrayList, Set, Map.
 //    Show in code the examples you mentioned.
 
+    //(2) DISPLAY ITEM NAME (for ex: School Supplies) Using ARRAY
+    println("----------------------------------------------------")
+    var schoolSupplies = arrayOf("pencil","bag","notebooks","crayons")
+    var counter:Int =0 //counter
+    while (counter<schoolSupplies.size){
+        println(schoolSupplies[counter])
+        counter++
+    }
+    println()
+    //(2) TO COMPUTE THE AVERAGE OF TOTAL AMOUNT OF ITEM Using ARRAYLIST
+    println("--------------------------------------------------------")
+    val amount = arrayListOf<Double>()
+    var ctr=1
+    var totalAmount:Double=0.0
+
+    repeat(5){
+        println("Enter item amount $ctr:")
+        amount.add(readln().toDouble())
+        ctr++
+    }
+    totalAmount = computeTotalAmount(amount)
+    println("Total Amount: ${String.format("%.2f",totalAmount)}\n")
+
+    println("--------------------------------------------------------")
+    //(3) DISPLAY RANKS USING MAPOF/MAP
+    val ranks = mapOf(1 to "China",2 to "India",3 to "United States",4 to "Indonesia")
+    println("-----Country with the Largest Population---")
+    println("Rank #1: "+ranks[1])
+    println("Rank #2: "+ranks[2])
+    println("Rank #3: "+ranks[3])
+    println("Rank #3: "+ranks[4])
+
+        }
+
+
+
+fun computeTotalAmount(number: ArrayList<Double>): Double {
+    val totalAmt = number.sum()
+    return totalAmt
+
 }
--- src/main/kotlin/kotlinactivities/activity02-C.kt
+++ src/main/kotlin/kotlinactivities/activity02-C.kt
@@ -1,3 +1,25 @@
 fun main(){
+    //Implement ACTIVITY01-C
+    var serialNumber:String ="123-MMMM"
+    var itemName:String ="Yogurt"
+    var category:String="Dairy"
+    var description:String ="Yogurt healthy drinks"
+    var quantity:Int=3
+    var price:Double =100.0
+    var isOutOfStock:Boolean =false
 
+
+    println("Enter product serial number")
+    serialNumber = readln()
+    println("Enter item Name")
+
+
+//sample
+//    println("Serial Number: $serialNumber")
+//    println("Name of the item: $itemName")
+//    println("Category: $category")
+//    println("Description: $description")
+//    println("Quantity: $quantity")
+//    println("Price: $price")
+//    println("Available?: $isOutOfStock")
 }
