2ec5794c7d7098b2fc39e8e08a845fb78e3555c7
Bernadette <bernadettedejesus99@gmail.com>
2023-05-18 21:35:11 +0800

    clean up

--- src/main/kotlin/ooppractices/ooppractice15/PartTimeEmployee.kt
+++ src/main/kotlin/ooppractices/ooppractice15/PartTimeEmployee.kt
@@ -1,11 +1,7 @@
 package ooppractices.ooppractice15
 
 class PartTimeEmployee(private val hourlyRate:Double,private val hoursOfWork:Double):Employee() {
-<<<<<<< HEAD
     //based on hours per rate and number of work :-)
-=======
-    //based on hours per work and number of work
->>>>>>> ca3a9b54529872aeda5de94135b7f82fe1ebc1a7
     override fun calculateEarnings(): Double {
         return   hoursOfWork * hourlyRate
     }
